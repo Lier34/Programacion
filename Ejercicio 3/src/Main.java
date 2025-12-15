5bc375b64c560cfe40c0224f9811abd9b9614233
Lier <lier.garrido.47469@ikasle.egibide.org>
2025-12-15 11:58:00 +0100

    Commit 3

--- Ejercicio 3/src/Main.java
+++ Ejercicio 3/src/Main.java
@@ -1,15 +1,48 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+       Scanner scanner = new Scanner(System.in);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+       System.out.println("-- menu --");
+       System.out.print("1. Añadir libro: ");
+       String titulo = scanner.next();
+       System.out.println("2. Consultar libro");
+       System.out.println("3. Eliminar libro");
+       System.out.println("4. Mostrar todos");
+       System.out.println("5. Verificar si esta vacia");
+       System.out.println("6. Salir");
+       System.out.print("Seleccione una opción (1-6): ");
+       int opcion = scanner.nextInt();
+
+       if (opcion == 1) {
+           System.out.println("Libro introducido en la parte superior de la pila");
+
+       } else if (opcion == 2) {
+           System.out.println("El titulo del libro es: " + titulo);
+
+       } else if (opcion == 3) {
+           System.out.println("El libro cuyo titulo es " + titulo + " ha sido eliminado");
+
+       } else if (opcion == 4) {
+           System.out.println("Estos son todos los libros en la pila:" + titulo);
+
+       } else if (opcion == 5) {
+           System.out.println("Verificando si hay libros en la pila...");
+
+           if (titulo.isEmpty()) {
+               System.out.println("La pila está vacía");
+           } else {
+               System.out.println("La pila no está vacía");
+           }
+
+       } else if (opcion == 6) {
+           System.out.println("Saliendo del programa...");
+           scanner.close();
+
+       } else {
+           System.out.println("Opción no válida");
+           main(args);
+       }
     }
 }
